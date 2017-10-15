3ec2f110713485846478b311db3b01853df51705
sitsky <aris.miliotis@gmail.com>
2017-10-15 17:56:47 -0400

    Initial commit

---
+++ Assets/New_Piece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Piece : MonoBehaviour {
+
+    public GameObject[] blocks;
+
+    // Use this for initialization
+    void Start () {
+        spawnNext();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnNext()
+    {
+        // Random Index
+        int i = Random.Range(0, blocks.Length);
+
+        // Spawn Group at current Position
+        Instantiate(blocks[i],
+                    transform.position,
+                    Quaternion.identity);
+    }
+}
